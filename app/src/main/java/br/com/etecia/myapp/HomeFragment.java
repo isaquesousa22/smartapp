5026a9680907c78a09891fe8b5af2accd680ecc1
isaquesousa22 <isaqueluzsousa83@gmail.com>
2025-12-01 21:11:17 -0300

    dsadsadsadsa

--- app/src/main/java/br/com/etecia/myapp/HomeFragment.java
+++ app/src/main/java/br/com/etecia/myapp/HomeFragment.java
@@ -1,20 +1,48 @@
 package br.com.etecia.myapp;
 
 import android.os.Bundle;
-
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-
 
 public class HomeFragment extends Fragment {
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.home_fragment, container, false);
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Toolbar toolbar = view.findViewById(R.id.toolbar);
+        ((AppCompatActivity) requireActivity()).setSupportActionBar(toolbar);
+
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        inflater.inflate(R.menu.top_app_bar, menu);  // AQUI CARREGA SEU MENU!
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.favorite) {
+        }
+
+        if (item.getItemId() == R.id.search) {
+        }
+
+        if (item.getItemId() == R.id.more) {
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
